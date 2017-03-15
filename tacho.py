aba9fff4a6ed6784a9e1c36dcdac4872a3530fc9
aleludziak <aleludziak@gmail.com>
2017-03-15 23:35:13 +0100

    it can add entry to the list and show it on the label, has numpad and plus button, code has some comments

--- tacho.py
+++ tacho.py
@@ -1,64 +1,97 @@
 # Panie wężu proszę o program do tacho
 from tkinter import *
 
+timeEntries = []
+info = ""
+def calc(evt): #For calculation button
+    global info
+    lb1.insert(END,e.get())
+    timeEntries.append(e.get())
+    info = e.get()
+    stat.set(info)
+    e.delete(0,END)
 
-def calc(evt):
+def clearbox(evt): # clear entry box
     e.delete(0,END)
     e.focus()
 
-def clearbox(evt):
-    e.delete(0,END)
-    e.focus()
-
-
+#=======tkinter window===========
 win = Tk()
-#win.geometry("775x325")
-win.wm_title("Tacho 0.0.1")
+#win.geometry("775x325") # Force window size
+win.wm_title("Tacho 0.0.2")
 win.resizable(width=FALSE, height=FALSE)
 
-pFrame = Frame(win)
-pFrame.grid(row=1,column=1)
+topFrame = Frame(win)
+#topFrame.pack(fill=BOTH)
+topFrame.grid(row=0,column=1)
+
+rightFrame = Frame(win)
+rightFrame.grid(row=1, column=1) # This puts item on place
+
+leftFrame = Frame(win)
+#middleFrame.pack(fill=BOTH)
+leftFrame.grid(row=1, column=0)
+
+downFrame = Frame(win)
+downFrame.grid(row=2,columnspan=2)
 
 v = StringVar()
-e = Entry(pFrame,textvariable=v,font = "Helvetica 20 bold", width=15, justify=RIGHT)
-e.grid(row=0, column=0, columnspan=6)
+e = Entry(topFrame, textvariable=v, font ="Helvetica 20 bold", width=15, justify=RIGHT)
+e.pack(fill=X, expand=True, side=RIGHT, ipady=10)
 e.bind('<Button-1>', clearbox)
 e.focus()
 
+#-------set what happens if you press Enter-----
 win.bind("<Return>", calc)
 win.bind("<KP_Enter>", calc)
+win.bind("<KP_Add>", calc) #have to be fixed adding "+" to the list
 
+#=========================================
 
+lb1 = Listbox(leftFrame, exportselection=0, height=15, width=40)
+lb1.pack(side=LEFT, fill=BOTH, expand=True)
+
+sb1 = Scrollbar(leftFrame, orient=VERTICAL) #scrollbar
+sb1.pack(side=LEFT,fill=BOTH)
+
+sb1.configure(command=lb1.yview)
+lb1.configure(yscrollcommand=sb1.set)
+
+# lb1.bind('<<ListboxSelect>>', firstselect) #action for selected line
+
+stat = StringVar()
+status = Label(downFrame, textvariable=stat, bd=1, relief=SUNKEN, font = "Helvetica 15 bold",width=50)
+stat.set(info)
+status.pack(fill=X, expand=True, side=TOP, ipady=10)
 
 def num_press(num):
         if num == "C":
-            e.delete(0,END)
-            e.focus()
-        elif num == ",":
+            clearbox("C")
+        elif num == ",": # yeah, I found it quite usefull for my keyboard
             e.insert(END,".")
         else:
 
             e.insert(END, num)
             e.focus()
+#=======Numpad==========
 bttn = []
 numbers="HMS789456123C0."
 i = 0
 for j in range(1,6):
     for k in range(3):
-        bttn.append(Button(pFrame, text = numbers[i], font = "Helvetica 15 bold", height = 1, width = 1))
+        bttn.append(Button(rightFrame, text = numbers[i], font ="Helvetica 15 bold", justify=LEFT, anchor=W, height = 1, width = 1))
         bttn[i].grid(row = j, column = k, pady = 2, padx = 2)
         bttn[i]["command"] = lambda x = numbers[i]: num_press(x)
         i += 1
 
+#other buttons
 
-
-cbutton = Button(pFrame, text = "=", font = "Helvetica 10 bold", height = 9, width = 9)
-cbutton.grid(row = 2, column = 5, columnspan = 4, rowspan = 4, pady = 2, padx=2)
+cbutton = Button(rightFrame, text ="+", font ="Helvetica 10 bold", height = 9, width = 9)
+cbutton.grid(row = 1, column = 5, columnspan = 5, rowspan = 4, pady = 2, padx=2)
 cbutton.bind('<Button-1>', calc)
 
-pbutton = Button(pFrame, text = "+", font = "Helvetica 10 bold", height = 4, width = 9)
-pbutton.grid(row = 0, column = 5, columnspan = 4, rowspan = 4, pady = 2, padx=2)
-pbutton.bind('<Button-1>', calc)
 
+
+#============================================
 
 win.mainloop()
